822374bbb2b177c55daeb10e1bf99db6c0973ba4
onyemaechiezichi-lang <onyemaechiezichi@gmail.com>
2025-11-11 10:48:04 +0100

    Fix: Task 3 - moved files to root and ensured context API with useState implemented correctly

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,5 @@
+import UserDetails from './UserDetails';
+function UserInfo() { // No need for userData prop
+  return <UserDetails />; // No need to pass userData as prop here
+}
+export default UserInfo;
