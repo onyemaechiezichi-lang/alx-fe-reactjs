f4dfb05e9b1958bbe78b3a51ffa69c2364cf2675
onyemaechiezichi-lang <onyemaechiezichi@gmail.com>
2025-11-14 16:48:13 +0100

    fix: Renamed editRecipe to updateRecipe per task requirement and ensured all files are committed

--- src/components/recipeStore.js
+++ src/components/recipeStore.js
@@ -13,13 +13,6 @@ export const useRecipeStore = create(
         set((state) => ({
           recipes: state.recipes.filter(recipe => recipe.id !== recipeId),
         })),
-      // NEW ACTION REQUIRED BY TASK: updateRecipe
-      updateRecipe: (recipeId, updatedRecipe) =>
-        set((state) => ({
-          recipes: state.recipes.map((recipe) =>
-            recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe
-          ),
-        })),
       setRecipes: (recipes) => set({ recipes }),
     }),
     {
