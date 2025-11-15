38806c34e59919e7b2f254a385f41e9750c924b4
onyemaechiezichi-lang <onyemaechiezichi@gmail.com>
2025-11-15 20:03:17 +0100

    commit task 1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,28 +1,15 @@
-import { Routes, Route } from 'react-router-dom'; // <-- NEW IMPORTS
-import AddRecipeForm from './components/AddRecipeForm.jsx';
-import RecipeList from './components/RecipeList.jsx';
-import RecipeDetails from './components/RecipeDetails.jsx'; // <-- NEW IMPORT
-
-// New Home Component to display the form and list on the root route
-const Home = () => (
-  <div>
-    <AddRecipeForm />
-    <RecipeList />
-  </div>
-);
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-
-      {/* Define the routes */}
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Route for individual recipe details, using the ID parameter */}
-        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
