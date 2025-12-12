b28fd29ce5773e391ec9fb3f7df667db67231e3e
onyemaechiezichi-lang <onyemaechiezichi@gmail.com>
2025-12-12 17:05:24 +0100

    tassss

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { useAuth } from './AuthContext';
 import { Navigate, useLocation } from 'react-router-dom';
-
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
   const location = useLocation();
@@ -12,8 +11,6 @@ const ProtectedRoute = ({ children }) => {
     // Redirect to the login page, but save the current location they tried to go to
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
-
   return children;
 };
-
 export default ProtectedRoute;
